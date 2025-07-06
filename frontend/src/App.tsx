556f1cdf3b3541266425f3a16297465a7897b64f
thiagoespindula00 <thiagoespindula00@gmail.com>
2025-07-06 16:30:36 -0300

    Commit inicial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {AppBar, Toolbar, Typography} from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography>Cadastro de pessoas físicas</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
+
+export default App;
