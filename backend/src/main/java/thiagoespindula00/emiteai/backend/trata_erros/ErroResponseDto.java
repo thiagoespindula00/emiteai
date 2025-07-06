a30da8770aa784a3736dce1fa64a4b375eac7daa
thiagoespindula00 <thiagoespindula00@gmail.com>
2025-07-06 16:17:06 -0300

    Criado dto para padronizar retorno de erros de regras de negócio

---
+++ backend/src/main/java/thiagoespindula00/emiteai/backend/trata_erros/ErroResponseDto.java
@@ -0,0 +1,4 @@
+package thiagoespindula00.emiteai.backend.trata_erros;
+
+public record ErroResponseDto(String mensagem) {
+}
--- backend/src/main/java/thiagoespindula00/emiteai/backend/trata_erros/TratadorDeErros.java
+++ backend/src/main/java/thiagoespindula00/emiteai/backend/trata_erros/TratadorDeErros.java
@@ -5,26 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class TratadorDeErros {
     @ExceptionHandler(ValidacaoException.class)
     public ResponseEntity<?> trataErroRegraDeNegocio(ValidacaoException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.badRequest().body(new ErroResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> trataErroRegraDeNegocioBeanValidation(MethodArgumentNotValidException exception) {
-        Map<String, String> errors = new HashMap<>();
+        var fieldError = exception.getBindingResult().getFieldErrors().get(0);
 
-        exception.getBindingResult().getFieldErrors().forEach((error) -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
-
-        return ResponseEntity.badRequest().body(errors);
+        return ResponseEntity.badRequest().body(new ErroResponseDto(fieldError.getField() + ": " + fieldError.getDefaultMessage()));
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
