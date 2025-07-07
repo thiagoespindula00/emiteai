848c3f282e5217d74ccb311453473b4b32aef25f
thiagoespindula00 <thiagoespindula00@gmail.com>
2025-07-06 22:38:39 -0300

    Incluído dockerfile para o backend e frontend

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: `http://${process.env.BACKEND_HOST_NAME || 'localhost'}:8080`,
     headers: {
         "Content-Type": "application/json"
     }
